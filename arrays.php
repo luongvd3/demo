3c302b8d4a960c360188daa003618e9d71df5557
Luong Tran <luongvd3@gmail.com>
2017-06-27 02:27:53 +1000

    end of lecture 11

---
+++ arrays.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<?php 
+$numberList = array(2,3,4,5,6,7,8, '5345', 345, '<h1>hello</h1>');
+
+//print_r($numberList);
+
+//echo $numberList[0];
+
+
+
+
+
+?>	
+
+</body>
+</html>
