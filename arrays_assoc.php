c7adb35e0abd14eecb0ee42297ecaff358aacfdb
Luong Tran <luongvd3@gmail.com>
2017-06-27 02:49:02 +1000

    end of lecture 12 and practice_app including

---
+++ arrays_assoc.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<?php 
+$number = array(10,20 ,49);
+
+//echo $number[2] . "<br>";
+print_r($number);
+
+echo "<br>";
+
+$names = array("first_name" =>'Edwin', "last_name" => 'Diaz');
+
+echo $names['first_name'] . " " . $names["last_name"];
+
+
+?>
+
+
+
+
+</body>
+</html>
